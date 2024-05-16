2e27a372d9489c045bfcc81f2584527f677c9ebe
sleechengn <sleechengn@live.com>
2024-05-16 01:11:35 +0000

    脚本添加

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+IMAGE_TAG=$1
+if [ $IMAGE_TAG ]; then
+		docker build $(dirname $0) --file Dockerfile -t $IMAGE_TAG
+	else
+		docker build $(dirname $0) --file Dockerfile -t sleechengn/comfyui:nvidia-gpu-base
+fi
